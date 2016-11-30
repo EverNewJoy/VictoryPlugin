ff47156a32554ba06b45c783b0d3859bd8c8788c
KrisRedbeard <krisrigby@gmail.com>
2016-11-30 23:13:14 +1100

    Update VictoryBPFunctionLibrary.h

--- Source/VictoryBPLibrary/Public/VictoryBPFunctionLibrary.h
+++ Source/VictoryBPLibrary/Public/VictoryBPFunctionLibrary.h
@@ -177,6 +177,53 @@ namespace EJoyGraphicsFullScreen
 	};
 }
 
+USTRUCT(BlueprintType)
+struct FLevelStreamInstanceInfo
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(Category = "LevelStreaming", BlueprintReadWrite)
+	FName PackageName;
+		
+	UPROPERTY(Category = "LevelStreaming", BlueprintReadWrite)
+	FName PackageNameToLoad;
+
+	UPROPERTY(Category = "LevelStreaming", BlueprintReadWrite)
+	FVector Location;
+
+	UPROPERTY(Category = "LevelStreaming", BlueprintReadWrite)
+	FRotator Rotation;
+
+	UPROPERTY(Category = "LevelStreaming", BlueprintReadWrite)
+	uint8 bShouldBeLoaded:1;
+
+	UPROPERTY(Category = "LevelStreaming", BlueprintReadWrite)
+	uint8 bShouldBeVisible:1;
+
+	UPROPERTY(Category = "LevelStreaming", BlueprintReadWrite)
+	uint8 bShouldBlockOnLoad:1;
+
+	UPROPERTY(Category = "LevelStreaming", BlueprintReadWrite)
+	int32 LODIndex;
+
+	FLevelStreamInstanceInfo() {}
+
+	FLevelStreamInstanceInfo(ULevelStreamingKismet* LevelInstance);
+
+	FString ToString() const
+	{
+		return FString::Printf(TEXT("PackageName: %s\nPackageNameToLoad:%s\nLocation:%s\nRotation:%s\nbShouldBeLoaded:%s\nbShouldBeVisible:%s\nbShouldBlockOnLoad:%s\nLODIndex:%i")
+			, *PackageName.ToString()
+ 			, *PackageNameToLoad.ToString()
+			, *Location.ToString()
+ 			, *Rotation.ToString()
+ 			, (bShouldBeLoaded) ? TEXT("True") : TEXT("False")
+ 			, (bShouldBeVisible) ? TEXT("True") : TEXT("False")
+ 			, (bShouldBlockOnLoad) ? TEXT("True") : TEXT("False")
+ 			, LODIndex);
+	}
+};
+
 
 UCLASS()
 class VICTORYBPLIBRARY_API UVictoryBPFunctionLibrary : public UBlueprintFunctionLibrary
@@ -1772,6 +1819,12 @@ static bool Capture2D_Project(class ASceneCapture2D* Target, FVector Location, F
 
 	UFUNCTION(Category = "VictoryBPLibrary|Team", BlueprintCallable)
 	static void SetGenericTeamId(AActor* Target, uint8 NewTeamId);
+	
+	UFUNCTION(Category = "LevelStreaming", BlueprintCallable)
+	static FLevelStreamInstanceInfo GetLevelInstanceInfo(ULevelStreamingKismet* LevelInstance);
+
+	UFUNCTION(Category = "LevelStreaming", BlueprintCallable, Meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject"))
+	static void AddToStreamingLevels(UObject* WorldContextObject, const FLevelStreamInstanceInfo& LevelInstanceInfo);
 
 //~~~~~~~~~
 
