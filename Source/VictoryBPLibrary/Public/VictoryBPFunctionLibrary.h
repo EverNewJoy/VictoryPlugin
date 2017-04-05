6f11be78df8f1a23375d1185391e0a25def48228
Rama <rama@evernewjoy.com>
2017-04-05 14:58:59 -0400

    Fix for various formatting errors in recent PR

--- Source/VictoryBPLibrary/Public/VictoryBPFunctionLibrary.h
+++ Source/VictoryBPLibrary/Public/VictoryBPFunctionLibrary.h
@@ -1732,7 +1732,6 @@ static void SetBloomIntensity(APostProcessVolume* PostProcessVolume,float Intens
 
 //~~~ Kris ~~~
 
-<<<<<<< HEAD
 	/* 
 	 *See if index is a valid index for this array
 	 *    
@@ -1742,18 +1741,7 @@ static void SetBloomIntensity(APostProcessVolume* PostProcessVolume,float Intens
 	*/
 	UFUNCTION(Category="VictoryBPLibrary|Utilities|Array", BlueprintPure, CustomThunk, meta=(DisplayName = "Valid Index", CompactNodeTitle = "VALID INDEX", ArrayParm = "TargetArray"))
 	static bool Array_IsValidIndex(const TArray<int32>& TargetArray, int32 Index);
-=======
-/* 
- *See if index is a valid index for this array
- *    
- *@param    TargetArray        The array to perform the operation on
- *@param    Index            The index to check.
- *@return    Bool if integer is valid index for this array
-*/
-UFUNCTION(Category="Victory BP Library|Utilities|Array", BlueprintPure, CustomThunk, meta=(DisplayName = "Valid Index", CompactNodeTitle = "VALID INDEX", ArrayParm = "TargetArray"))
-static bool Array_IsValidIndex(const TArray<int32>& TargetArray, int32 Index);
->>>>>>> a0373028600db9be19dcba75ace78574b0b5334d
-
+	 
 	static bool GenericArray_IsValidIndex(void* TargetArray, const UArrayProperty* ArrayProp, int32 Index);
 	  
 	DECLARE_FUNCTION(execArray_IsValidIndex)
@@ -1774,7 +1762,6 @@ static bool Array_IsValidIndex(const TArray<int32>& TargetArray, int32 Index);
 		*(bool*)RESULT_PARAM = WasValid;
 	}
 
-<<<<<<< HEAD
 	/** Get the time target actor was created. */
 	UFUNCTION(Category = "VictoryBPLibrary|Actor", BlueprintPure, Meta = (DefaultToSelf = "Target"))
 	static float GetCreationTime(const AActor* Target);
@@ -1790,24 +1777,7 @@ static bool Array_IsValidIndex(const TArray<int32>& TargetArray, int32 Index);
 	/** Contributed by Community Member Kris! */
 	UFUNCTION(Category = "VictoryBPLibrary|SceneCapture", BlueprintPure, Meta = (DefaultToSelf = "Target"))
 	static bool Capture2D_Project(class ASceneCapture2D* Target, FVector Location, FVector2D& OutPixelLocation);
-=======
-/** Get the time target actor was created. */
-UFUNCTION(Category = "Victory BP Library|Actor", BlueprintPure, Meta = (DefaultToSelf = "Target"))
-static float GetCreationTime(const AActor* Target);
 
-/** Get the time target actor has been alive. */
-UFUNCTION(Category = "Victory BP Library|Actor", BlueprintPure, Meta = (DefaultToSelf = "Target"))
-static float GetTimeAlive(const AActor* Target);
-	
-/** Contributed by Community Member Kris! */
-UFUNCTION(Category = "Victory BP Library|SceneCapture", BlueprintPure)
-static bool CaptureComponent2D_Project(class USceneCaptureComponent2D* Target, FVector Location, FVector2D& OutPixelLocation);
-  
-/** Contributed by Community Member Kris! */
-UFUNCTION(Category = "Victory BP Library|SceneCapture", BlueprintPure, Meta = (DefaultToSelf = "Target"))
-static bool Capture2D_Project(class ASceneCapture2D* Target, FVector Location, FVector2D& OutPixelLocation);
->>>>>>> a0373028600db9be19dcba75ace78574b0b5334d
-     
 	/** Make sure to include the appropriate image extension in your file path! Recommended: .bmp, .jpg, .png. Contributed by Community Member Kris! */
 	UFUNCTION(Category = "Victory BP Library|SceneCapture", BlueprintCallable)
 	static bool CaptureComponent2D_SaveImage(class USceneCaptureComponent2D* Target, const FString ImagePath, const FLinearColor ClearColour);
@@ -1926,7 +1896,7 @@ static bool Capture2D_Project(class ASceneCapture2D* Target, FVector Location, F
 	 *
 	 * @return True if OutViewportPosition is not 0,0.
 	 */
-	UFUNCTION(Category = "VictoryBPLibrary|Game|Viewport", BlueprintCallable)
+	UFUNCTION(Category = "VictoryBPLibrary|Game|Viewport", BlueprintCallable, meta=(WorldContext="WorldContextObject"))
 	static bool GetViewportPosition(UObject* WorldContextObject, const FVector2D& ScreenPosition, FVector2D& OutViewportPosition);
 
 	/**
@@ -1941,7 +1911,7 @@ static bool Capture2D_Project(class ASceneCapture2D* Target, FVector Location, F
 	 *
 	 * @return True if there was a hit, false otherwise.
 	 */
-	UFUNCTION(Category = "VictoryBPLibrary|Game|Viewport", BlueprintCallable, Meta = (bTraceComplex = true, TraceChannel = ECC_Visibility))
+	UFUNCTION(Category = "VictoryBPLibrary|Game|Viewport", BlueprintCallable, Meta = (WorldContext="WorldContextObject", bTraceComplex = true, TraceChannel = ECC_Visibility))
 	static bool GetViewportPositionHitResultByChannel(UObject* WorldContextObject, const FVector2D& ViewportPosition, ECollisionChannel TraceChannel, bool bTraceComplex, FHitResult& OutHitResult);
 
 	/**
@@ -1954,7 +1924,7 @@ static bool Capture2D_Project(class ASceneCapture2D* Target, FVector Location, F
 	 *
 	 * @return false if something went wrong during the deproject process.
 	 */
-	UFUNCTION(Category = "VictoryBPLibrary|Game|Viewport", BlueprintCallable)
+	UFUNCTION(Category = "VictoryBPLibrary|Game|Viewport", BlueprintCallable, Meta = (WorldContext="WorldContextObject"))
 	static bool ViewportPositionDeproject(UObject* WorldContextObject, const FVector2D& ViewportPosition, FVector& OutWorldOrigin, FVector& OutWorldDirection);
 	
 	/**
