c3461a825cd334a4455e9cd1952735610c55f2af
Joshua Harrington <mail@joshuaharrington.co.nz>
2017-09-10 15:12:41 +1200

    Gameframework changed to GameFramework

--- Source/VictoryBPLibrary/Public/VictoryPC.h
+++ Source/VictoryBPLibrary/Public/VictoryPC.h
@@ -8,7 +8,7 @@
 //HTTP
 #include "Http.h"
 
-#include "Runtime/Engine/Classes/Gameframework/PlayerController.h"
+#include "Runtime/Engine/Classes/GameFramework/PlayerController.h"
 #include "VictoryPC.generated.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(VictoryPCLog, Log, All);
